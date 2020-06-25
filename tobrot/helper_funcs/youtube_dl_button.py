c008011f9989969444ba308de7cab4095369a046
SerMax20 <67185027+SerMax20@users.noreply.github.com>
2020-06-25 09:24:01 +0000

    Update youtube_dl_button.py

--- tobrot/helper_funcs/youtube_dl_button.py
+++ tobrot/helper_funcs/youtube_dl_button.py
@@ -85,7 +85,7 @@ async def youtube_dl_call_back(bot, update):
     LOGGER.info(custom_file_name)
     #
     await update.message.edit_caption(
-        caption="trying to download"
+        caption="🕛trying to download"
     )
     description = "@PublicLeech"
     if "fulltitle" in response_json:
