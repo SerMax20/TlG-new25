a77896ca1541379781e9f4842d1e0141d64d1127
SerMax20 <67185027+SerMax20@users.noreply.github.com>
2020-06-25 09:30:16 +0000

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,7 +38,33 @@ async def help_message_f(client, message):
     #message_id = 99
     # display the /help
     
-    await message.reply_text("""Thanks join this group Please Read Pinned Massage 🤣""", disable_web_page_preview=True)
+    await message.reply_text("""෴❤️ආයුබෝවන්❤️෴\n\n 🔥/ytdl: This command should be used as reply to a supported link\n\n 
+
+🔥/leech: This command should be used as reply to a magnetic link, a torrent link, or a direct link. [this command will SPAM the chat and send the downloads a seperate files, if there is more than one file, in the specified torrent]\n\n 
+
+🔥/leech archive: This command should be used as reply to a magnetic link, a torrent link, or a direct link. [This command will create a .tar.gz file of the output directory, and send the files in the chat, splited into PARTS of 1024MiB each, due to Telegram limitations]\n\n 
+
+🔥/gleech: This command should be used as reply to a magnetic link, a torrent link, or a direct link.\n\n 
+
+🔥/leech unzip: This will unzip the .zip file and dupload to telegram.\n\n 
+
+🔥/gleech unzip: This will unzip the .zip file and upload to drive.\n\n 
+
+🔥/leech unrar: This will unrar the .rar file and dupload to telegram.\n\n 
+
+🔥/gleech unrar: This will unrar the .rar file and upload to drive.\n\n 
+
+🔥/leech untar: This will untar the .tar file and upload to telegram.\n\n 
+
+🔥/gleech untar: This will untar the .tar file and upload to drive.\n\n 
+
+🔥/tleech: This will mirror the telegram files to ur respective cloud drive.\n\n 
+
+🔥/tleech unzip: This will unzip the .zip telegram file and upload to drive.\n\n 
+
+🔥/tleech unrar: This will unrar the .rar telegram file and upload to drive.\n\n 
+
+🔥/tleech untar: This will untar the .tar telegram file and upload to drive\n\n """, disable_web_page_preview=True)
 
 
 async def rename_message_f(client, message):
